10eb9d94669a8efea9388bce0f89bbd969b2e2f1
George Lioympas <g.lioympas@gmail.com>
2018-11-09 04:03:40 +0200

    a bit of refactoring, deleting images after generating the gif

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -22,10 +22,8 @@ class ImageController extends Controller
     {
 
     	//Validation to avoid exploiting and etc. Think that a user can upload a .php in your server, that's baaaaaad. ONLY IMAGES
-    	//validation becomes with FuleUploadRequest request.
-
-
-    	
+    	//validation becomes with FileUploadRequest request.
+   	
     	$file = $request->file('file');
     	$name = time() . $file->getClientOriginalName();
     	$file->move('images',$name);
@@ -39,7 +37,7 @@ class ImageController extends Controller
     {
     	$names = $request->names;
 
-    	//add /public/images/ to every image image:D , so the final will be /public/images/exampleName.png
+    	//add /public/images/ to every image image , so the final will be /public/images/exampleName.png
     	$names = array_map(function($name){
     		return public_path() . '/images/' . $name; 
     	} , $names);
@@ -48,13 +46,17 @@ class ImageController extends Controller
     	//fill the durations array as many times are are the images.
     	$durations = [];
     	foreach($names as $i)
-    		$durations[] = 150;
+    		$durations[] = 150; 
 
     	$gc = new GifCreator();
     	$gc->create($names , $durations , 0);
     	$gifBinary = $gc->getGif();
     	$gifName = time();
     	file_put_contents(public_path() .'/images/gifs/' . $gifName . '.gif', $gifBinary); //saving the gif to public/images/gifs/date.gif
+
+        //delete the old-images to free memory
+        foreach($names as $img)
+            unlink($img);
 
     	return response()->json(['created'=>URL::to('/images/gifs/'.$gifName.'.gif')] , 200);
     }
